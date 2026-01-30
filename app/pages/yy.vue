b417b4053ac05aaa6cec4d771cb80612a03b6838
최정화 <8purity7@gmail.com>
2026-01-30 20:13:06 +0900

    여백 수정

--- app/pages/yy.vue
+++ app/pages/yy.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import Card from '~/components/CardEn.vue'
 
-const title = '❤️ Aunt Yunyoung & Uncle Max ❤️'
+const title = '❤️Aunt Yunyoung & Uncle Max❤️'
 const mainText = 'Hi! I’m <span class="text-purple-600 font-bold">Bling</span>.'
-const subText = 'I’m growing healthy and strong every day!<br/>\
+const subText = 'I’m growing healthy <br/>\ and strong every day!<br/>\<br/>\
   We’ll meet in <span class="font-semibold text-xl">September</span> 💕'
 const footerText = 'I can’t wait to see you. Love you! ♥️'
 const babyImage = '/bling-baby.png'
